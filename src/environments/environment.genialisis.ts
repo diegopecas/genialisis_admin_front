29d0d8b92ea1dca4f54fdf8c65e2e81ba7845cde
pilarrestrepo <pilar.restrepo@live.com>
2026-08-05 16:03:39 -0500

    Update environment.genialisis.ts

--- src/environments/environment.genialisis.ts
+++ src/environments/environment.genialisis.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "https://api.genialisis.com/",
+  api: "https://api.clientes.genialisis.com/",
   claudeApiUrl: 'https://api.anthropic.com/v1/messages',
   claudeApiKey: 'tu-api-key-aquí',
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent',
